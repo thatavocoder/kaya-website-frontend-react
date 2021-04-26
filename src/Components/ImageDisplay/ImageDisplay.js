bb4f8e287c30bfe63e4c12ff5e68ff9d09648bc9
Aashka Doshi <aashkadoshi7@gmail.com>
2021-04-26 22:37:11 +0530

    added pages in start diagnosis

--- src/Components/ImageDisplay/ImageDisplay.js
+++ src/Components/ImageDisplay/ImageDisplay.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import acnefacials from './Images/acnefacials.png'
+import './CSS/ImageDisplay.css'
 
 export default function ImageDisplay() {
     return (
@@ -12,11 +13,22 @@ export default function ImageDisplay() {
                 <div className="imgheader col s10">
                 </div>
             </div>
-            <div className='displayimg'>
-                <img src={acnefacials}></img>
-            </div>
-            <div className='deletebtn'>
-                <Link><i class="material-icons-outlined">delete</i><p>Delete</p></Link>
+            <div className='grid-container row'>
+                <div className='displayimg col s12 m12 l6 center-align'>
+                    <img src={acnefacials}></img>
+                </div>
+                <div className='imgeditbtns col s12 m12 l6'>
+                    <div className='deletebtn valign-wrapper'>
+                        <Link><span><i class="material-icons">delete</i>Delete</span></Link>
+                    </div>
+                    <p className='or'>OR</p>
+                    <div className='otherimgbtn  valign-wrapper'>
+                        <Link><span><i class="material-icons">photo_camera</i>Upload another image</span></Link>
+                    </div>
+                    <div className='imgdispnext  valign-wrapper'>
+                        <Link><span>Next &nbsp; &nbsp; &gt;</span></Link>
+                    </div>
+                </div>
             </div>
         </div>
     )
